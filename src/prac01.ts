af8f6d3e2ec6f128556c1860927f7dd52e4bc897
rodina <131938022+ro-dina@users.noreply.github.com>
2024-10-10 10:30:53 +0900

    dai2kai

--- src/prac01.ts
+++ src/prac01.ts
@@ -1,8 +1,10 @@
 export {};
-import { date2str } from "./utils/date2str"; // 関数date2strをインポート
+import dayjs from "dayjs"; // dayjsのインポート
 
 const deadline: Date = new Date(2024, 10, 2, 11, 45);
-const cratedAt: Date = new Date(); // 引数なしで現在日時を取得
+const createdAt: Date = new Date(); // 引数なしで現在日時を取得
 
-let str = `期限 ${date2str(deadline)} (登録日 ${date2str(cratedAt)})`;
+const str =
+  `期限 ${dayjs(deadline).format("YYYY/MM/DD HH:mm")}` +
+  `(登録日 ${dayjs(createdAt).format("YYYY/MM/DD HH:mm")})`;
 console.log(str);
